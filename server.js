0b80d80c00b08fe57722527f6192b7cb7af3eb95
Delilah <ila0406@gmail.com>
2022-04-04 17:53:38 -0600

    added handlebar partials

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: process.env.SECRET || 'supersecret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
