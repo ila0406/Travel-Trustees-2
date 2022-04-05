9fd223b0660adba05c1de03e9bb79ba884aaf46b
David faidley <david.faidley23@gmail.com>
2022-04-04 20:45:53 -0600

    added relationships to index.js in models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Airport = require('./Airport');
 const Weather = require('./Weather');
 const Safety = require('./Safety');
 
-User.hasMany(Reviews, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+User.hasMany(Reviews, { foreignKey: 'user_id' });
 
-Reviews.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+User.hasMany(Covid, { foreignKey: 'user_id' });
+
+User.hasMany(Airport, { foreignKey: 'user_id' });
+
+User.hasMany(Weather, { foreignKey: 'user_id' });
+
+User.hasMany(Safety, { foreignKey: 'user_id' });
+
+Reviews.belongsTo(User, { foreignKey: 'user_id' });
+
+Covid.belongsTo(User, { foreignKey: 'user_id' });
+
+Airport.belongsTo(User, { foreignKey: 'user_id' });
+
+Weather.belongsTo(User, { foreignKey: 'user_id' });
+
+Safety.belongsTo(User, { foreignKey: 'user_id' });
 
 module.exports = { User, Reviews, Covid, Airport, Weather, Safety };
