8d40515019d8d3b6f3b50935f4cc4519491d3428
Elbashadore <joshtibbetts12@gmail.com>
2022-04-09 11:43:40 -0600

    Render drop down

--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -1,10 +1,9 @@
+const {Airport, Country} = require('../../models/')
 const router = require('express').Router();
 
-
-
-router.get('/search',async (req,res)=>{
+router.get('/',async (req,res)=>{
   try {
-      const airportData = await Airport.findAll()
+      const airportData = await Country.findAll()
       res.status(200).json(airportData);
   } catch (err) {res.status(500).json(err)}
 });
